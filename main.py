ac217b676adee77aea99774ce9728649f21b4572
Devin Capriola <devincapriola@gmail.com>
2023-08-16 19:23:09 -0400

    Update main and chatbot functions

--- main.py
+++ main.py
@@ -2,29 +2,33 @@ import openai
 import gradio
 
 openai.api_key = "sk-xxxxxxxx"
-messages = [{
-    "role": "system",
-    "content": "You are a helpful and kind AI Assistant."}]
 
-
-def chatbot(input):
+def chatbot(input, messages=None):
+    if messages is None:
+        messages = []
     if input:
         messages.append({"role": "user", "content": input})
         chat = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=messages
         )
-        reply = chat.choices[0].message.content
+        reply = chat.choices[0].message['content']
         messages.append({"role": "assistant", "content": reply})
-        return reply
-
+        return reply, messages
 
 def main():
     inputs = gradio.inputs.Textbox(lines=12, label="Chat with the bot here")
     outputs = gradio.outputs.Textbox(label="Chatbot will reply here")
-    gradio.Interface(fn=chatbot, inputs=inputs, outputs=outputs, title="Chatbot",
-                     description="Chatbot powered by OpenAI GPT-3", theme="compact").launch(share=True)
-
+    
+    interface = gradio.Interface(
+        fn=chatbot,
+        inputs=inputs,
+        outputs=outputs,
+        title="Chatbot",
+        description="Chatbot powered by OpenAI GPT-3",
+        theme="compact"
+    )
+    interface.launch(share=True)
 
 if __name__ == '__main__':
     main()
